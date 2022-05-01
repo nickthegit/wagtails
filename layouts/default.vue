9b108d3d800e62aa1138f83f5196f567952a13b4
Nick <nick@rosielee.co.uk>
2022-05-01 14:38:17 +0100

    start nav and homepage

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <elements-header />
+    <Nuxt />
+    <elements-navigation />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+}
+</script>
+
+<style lang="scss" scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,10 @@
 <template>
-  <main><h1>Hi there david and frank</h1></main>
+  <main>
+    <h1>
+      <span>Beautiful</span> 2 Bedroom, Detached Country Holiday Let in
+      <span>Stunning</span> Dorset
+    </h1>
+  </main>
 </template>
 
 <script>
@@ -7,3 +12,25 @@ export default {
   name: 'IndexPage',
 }
 </script>
+
+<style lang="scss" scoped>
+main {
+  width: 100%;
+  height: 100vh;
+  position: relative;
+  padding: var(--edge-spacing);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+}
+h1 {
+  width: 100%;
+  max-width: 990px;
+  display: block;
+  font-size: var(--h1-sizing);
+  span {
+    color: var(--color-primary);
+  }
+}
+</style>
